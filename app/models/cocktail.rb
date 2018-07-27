52eb2015970a84dcbb8653e5022d58b31523d210
Luiz Guilherme Lins <lins.luizguilherme@gmail.com>
2018-07-27 14:44:06 -0300

    implement

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-
+  accepts_nested_attributes_for :doses
   validates :name, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
 Cocktail.create(name: "Vitinho's Mindpunch")
+Cocktail.create(name: "Cocktail D'Luiz")
+Cocktail.create(name: "+1 Drink")
